5c7fe0ae26cb07808551af57ea421abf3d650311
sylvainpeter <lexriders@gmail.com>
2022-08-12 10:44:15 +0200

    Ajoute Rate Limit

---
+++ backend/middleware/rate-limit.js
@@ -0,0 +1,11 @@
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 5, // Limite chaque IP à 5 requêtes par "fenêtre" (ici, par 15 minutes)
+	message: "Trop de tentatives de connexion depuis cette adresse IP, veuillez réessayer dans 15 minutes", //
+	standardHeaders: true,
+	legacyHeaders: false,
+})
+
+module.exports = limiter;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('../middleware/rate-limit');
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', rateLimit, userCtrl.signup);
+router.post('/login', rateLimit, userCtrl.login);
 
 module.exports = router;
